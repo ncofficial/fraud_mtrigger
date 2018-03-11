2c0e7577505ec8a6dfd78c20a560d763a168caca
ncofficial <36410310+ncofficial@users.noreply.github.com>
2018-03-12 02:54:27 +0500

    FMTrigger 1.0.1

--- js/index.js
+++ js/index.js
@@ -6,5 +6,25 @@ $('form').card({
     formSelectors: {
         nameInput: 'input[name="first-name"], input[name="last-name"]'
     }
-});
-/* Main trigger script *.
+})
+
+/* Main trigger script */
+
+var mouse_changes = 0
+
+function mouse_currentXY(e){
+	$($(".body-text")[0]).text("mouse location: "+ e.clientX+"  "+ e.clientY+"  "+mouse_changes+"  "+parse_mousecheck())
+	mouse_changes++
+}
+function parse_mousecheck(){
+	let parse_state = true
+	if (mouse_changes!==0){
+		parse_state = false
+	}
+	return parse_state
+}
+/* Call main script */
+
+$("document").ready(function(){
+	$("body").attr("onmousemove", "mouse_currentXY(event)")
+})
