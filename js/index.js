67307eb33e3d44468f675d4c9965e37113f427b8
ncofficial <36410310+ncofficial@users.noreply.github.com>
2018-03-13 22:49:48 +0500

    FMTrigger 1.1.0
    Neural network added, but looking for dataset.

--- js/index.js
+++ js/index.js
@@ -8,23 +8,82 @@ $('form').card({
     }
 })
 
+var current_history = [[],[],[]]
+function mouse_currentXY(e){
+	current_history[0]=(current_history[0]).concat([e.clientX])
+	current_history[1]=(current_history[1]).concat([e.clientY])
+	current_history[2]=(current_history[2]).concat(["time"])
+}
+$(document).ready(function() {
+$("body").attr("onmousemove", "mouse_currentXY(event)")
+
+// Mouse detect neural network
+
+var Neuron = synaptic.Neuron,
+	Layer = synaptic.Layer,
+	Network = synaptic.Network,
+	Trainer = synaptic.Trainer,
+	Architect = synaptic.Architect
+var inputLayer = new Layer(4)
+var hiddenLayer = new Layer(6)
+var outputLayer = new Layer(1)
+
+inputLayer.project(hiddenLayer)
+hiddenLayer.project(outputLayer)
+
+var mouseTrigger = new Network({
+	input: inputLayer,
+	hidden: [hiddenLayer],
+	output: outputLayer
+})
+
+// Loading dataset for network
+var fraud_history = [
+	[[],[],[]],
+	[[],[],[]],
+	[[],[],[]],
+	[[],[],[]],
+	[[],[],[]]
+]
+var holder_history = [
+	[[],[],[]],
+	[[],[],[]],
+	[[],[],[]],
+	[[],[],[]],
+	[[],[],[]]
+]
+var learningRate = .3
+for (var i = 0; i < 20000; i++)
+{
+	// fraud => 1, !fraud => 0
+	for(var j = 0; j < fraud_history.lenght; j++){
+		mouseTrigger.activate([fraud_history[j]])
+		mouseTrigger.propagate(learningRate, [1])
+		mouseTrigger.activate(holder_history[j])
+		mouseTrigger.propagate(learningRate, [0])
+	}
+}
+
+
+// test the network
+mouseTrigger.activate(fraud_history[0]) // must be [1]
+mouseTrigger.activate(holder_history[0]) // must be [0]
+
 /* Main trigger script */
 
-var mouse_changes = 0
 
-function mouse_currentXY(e){
-	$($(".body-text")[0]).text("mouse location: "+ e.clientX+"  "+ e.clientY+"  "+mouse_changes+"  "+parse_mousecheck())
-	mouse_changes++
-}
-function parse_mousecheck(){
-	let parse_state = true
-	if (mouse_changes!==0){
-		parse_state = false
-	}
-	return parse_state
-}
 /* Call main script */
 
-$("document").ready(function(){
-	$("body").attr("onmousemove", "mouse_currentXY(event)")
+$("#input-button").click(function() {
+	//check for count of mousemoves
+	if($(current_history[0]).size()!=0){
+		$($(".body-text")[0]).text("Good human")
+	}else{
+		$($(".body-text")[0]).text("Bad robot")
+	}
+	console.log(current_history)
+	console.log(mouseTrigger.activate(current_history))
+	$(".body-text").removeClass("none")
+	//check by neural network history of mousechanges
+})
 })
